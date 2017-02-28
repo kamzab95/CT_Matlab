1a258dd13f90150191ab0c8005d1a53641b59e6b
kamzab95 <drazz3r@gmail.com>
2017-02-28 15:43:11 +0100

    con
    as

--- control1_1.m
+++ control1_1.m
@@ -1,19 +1,27 @@
+%a
 angle = pi/5;
 angles = 0:angle:pi*2;
 pole = exp(complex(0, 1)*angles);
 
 [s,e]=size(pole);
+itr = 1;
 new_poles = [];
 for a=1:e
     if real(pole(a)) < 0
-        new_poles(a) = pole(a);
-        disp(pole(a))
+        new_poles(itr) = pole(a);
+        itr = itr+1;
     end
 end
-    
-%plot(real(pole), imag(pole))
+new_poles
+figure(3);
+plot(real(pole), imag(pole));
 [num, den] = zp2tf([], new_poles, 1);
 res = tf(num, den);
 pzmap(res);
-%pzmap(pole)
+
+%b
+figure(2);
+step(res);
+
+%test
 
