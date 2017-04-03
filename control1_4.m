b85fdaaa2a87ea89333ed162ce96f569a9fc2a29
kamzab95 <drazz3r@gmail.com>
2017-04-03 10:20:30 +0200

    up
    uo

--- control1_4.m
+++ control1_4.m
@@ -1,10 +1,15 @@
 s = tf('s');
 sys = 8/((s+0.01)*(s+0.2)*(s+1));
-rlocus(sys);
+%rlocus(sys);
 sys
-sisotool(sys);
+%sisotool('rlocus', sys);
+pidTuner(sys);
 
 %Ziuta = 0.707;
 %W = 1/(Ziuta*5);
 %W
 %sgrid(Ziuta, W);
+
+%Ziegler–Nichols
+%PID 
+%Ki = 0.38 Ti = 6.84 Tu = 1.71
