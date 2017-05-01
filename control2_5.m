2f7332c868d5912c76a239ab76ba24baacde0550
szymongaga <Szymonskowronek@wp.pl>
2017-05-01 16:37:52 +0200

    Add files via upload

--- control2_5.m
+++ control2_5.m
@@ -20,7 +20,15 @@ control_and_observ('Ex5 s2', A2, B2, C2, D2);
 [num2 ,den2] = ss2tf(A2, B2, C2, D2);
 tf2 = tf(num2, den2)
 
-% d ???
-step(tf2)
+% d 
+step(tf2);
+xd=pole(tf2)
+
+pol=[-0.2-0.2i;-0.2+0.2i;-0.2500+0.1936i;-0.2500-0.1936i]
+K=acker(A2,B2,pol)
+
+[NUM,DEN]=ss2tf(A2-B2*K,B2,C2,D2);
+sysx=tf(NUM,DEN);
+step(sysx)
 
 % e ???
